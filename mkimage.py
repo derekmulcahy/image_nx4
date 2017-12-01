11aa5d6130437c2ebc1340ba077ab27ca785a8b6
Derek Mulcahy <derekmulcahy@gmail.com>
2017-12-01 14:27:39 -0500

    Traversed pixels in correct order. Added nx4_testcard.png.

--- mkimage.py
+++ mkimage.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 import sys
 import pygame
-pygame.init()
-name = "pacman.png"
-image = pygame.image.load(name)
 
-def printxgs(r, g, b, x, o, f):
-    for i in range(o, len(r), 32):
-        sr = '_'.join(r[i:i+16])
-        sg = '_'.join(g[i:i+16])
-        sb = '_'.join(b[i:i+16])
-        print("wire [0:575] %sgs%-2d = {576'h%s};" % (x, (i-o)/32, '__'.join([sr, sg, sb])), sep='', file=f)
+class MkImage:
 
-(r, g, b) = ([], [], [])
-pixels = [("%03X" % d) for d in pygame.image.tostring(image, "RGB")]
-for i in range(0, len(pixels), 3):
-    b.append(pixels[i])
-    g.append(pixels[i+1])
-    r.append(pixels[i+2])
-f = open('image.v', 'w')
-printxgs(r, g, b, "l", 16, f)
-printxgs(r, g, b, "r",  0, f)
-f.close()
+    def print_gs(pixels, prefix, offset, has_alpha, f):
+        skip = 4 if has_alpha else 3
+        b = (pixels[0::skip])[::-1]
+        g = (pixels[1::skip])[::-1]
+        r = (pixels[2::skip])[::-1]
+        for i in range(offset, len(r), 32):
+            sr = '_'.join(r[i:i+16])
+            sg = '_'.join(g[i:i+16])
+            sb = '_'.join(b[i:i+16])
+            print("wire [0:575] %sgs%-2d = {576'h%s};" % (prefix, 35-((i-offset)/32), '__'.join([sr, sg, sb])), sep='', file=f)
+
+    def process():
+        pygame.init()
+
+        name = "jw.png" if len(sys.argv) == 1 else sys.argv[1]
+        image = pygame.image.load(name)
+
+        try:
+            pixels = [("%03X" % d) for d in pygame.image.tostring(image, "RGBA_PREMULT")]
+            has_alpha = True
+        except:
+            pixels = [("%03X" % d) for d in pygame.image.tostring(image, "RGB")]
+            has_alpha = False
+
+        f = open('image.v', 'w')
+        MkImage.print_gs(pixels, "l", 16, has_alpha, f)
+        MkImage.print_gs(pixels, "r",  0, has_alpha, f)
+        f.close()
+
+MkImage.process()
